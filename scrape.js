d1dbe3fa9061e77595e45109c9adefba1f6c837d
Sukrit022 <145593833+Sukrit022@users.noreply.github.com>
2024-07-05 23:56:22 +0530

    scrape.js

--- scrape.js
+++ scrape.js
@@ -2,18 +2,18 @@ import { Builder, By, until } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome.js';
 import fs from 'fs';
 
-function replacer(key,value){
-  if(key=="price"){
-    let val=value.substr(value.length-8,8);
-    // let j=0;
-    let val1="";
-    for(let i=0;i<val.length;i++){
-      if(val[i]!=','){
-        val1+=val[i];
-      }
+function replacer(key, value) {
+  if (key == "price") {
+    const pricePattern = /Rs\.\s*([\d,]+)/i;
+    const match = value.match(pricePattern);
+
+    if (match) {
+      const priceString = match[1].replace(/,/g, '');
+      const priceNumber = Number(priceString);
+      return priceNumber;
+    } else {
+      return null;
     }
-    let a=Number(val1)
-    return a;
   }
   return value;
 }
@@ -24,19 +24,24 @@ async function scrape() {
   try {
     await driver.get('https://www.nvidia.com/en-in/geforce/buy/');
 
-    let gpus = await driver.findElements(By.id('container-0c56a80eb9'));
+    let gpus = await driver.findElements(By.className('aem-Grid aem-Grid--10 aem-Grid--default--10 aem-Grid--phone--10 '));
+
+
+    console.log(`Found ${gpus.length} GPU elements`);
     let gpuData = [];
     console.log(gpus);
+
     for (let gpu of gpus) {
       let name = await gpu.findElement(By.className('title')).getText();
+
       let priceElement = await gpu.findElement(By.className('startingprice'));
       let price = priceElement ? await priceElement.getText() : 'NULL';
-      
-      gpuData.push({name, price});
+
+      gpuData.push({ name, price });
     }
 
     fs.writeFileSync('gpuData.json', JSON.stringify(gpuData, replacer, 2));
-    
+
   } finally {
     await driver.quit();
   }
